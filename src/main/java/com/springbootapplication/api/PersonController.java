6c8d5357045ff693039b34444e0a461506454560
Antonio Cabrera <acabrerag94@gmail.com>
2022-04-11 23:38:21 +0200

    GET and POST implemented

---
+++ src/main/java/com/springbootapplication/api/PersonController.java
@@ -0,0 +1,29 @@
+package com.springbootapplication.api;
+
+import com.springbootapplication.model.Person;
+import com.springbootapplication.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("api/v1/persons")
+@RestController
+public class PersonController {
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(@RequestBody Person person){
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPerson(){
+        return personService.getAllPerson();
+    }
+}
---
+++ src/main/java/com/springbootapplication/service/PersonService.java
@@ -0,0 +1,28 @@
+package com.springbootapplication.service;
+
+import com.springbootapplication.dao.PersonDao;
+import com.springbootapplication.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Service
+public class PersonService {
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPerson(){
+        return personDao.selectAllPerson();
+    }
+}
